1ba995f5a588e92a882bcd408cb7c960a654aa2c
BoyJayy <popkov.ad@icloud.com>
2026-01-04 19:35:55 +0300

    Add Sieve of Eratosthenes implementation
    Implement Sieve of Eratosthenes algorithm to find prime numbers up to a given limit.

---
+++ math/sieve.go
@@ -0,0 +1,22 @@
+package main
+
+
+func sieve(n int) []bool {
+	isPrime := make([]bool, n+1)
+	for i := 2; i <= n; i++ {
+		isPrime[i] = true
+	}
+	for i := 2; i*i <= n; i++ {
+		if isPrime[i] {
+			for j := i * i; j <= n; j += i {
+				isPrime[j] = false
+			}
+		}
+	}
+	return isPrime
+}
+
+func main() {
+	isPrime := sieve(1000000)
+	
+}
